33bd40885ea16dd04f9bfdb3e99196b5773d5f83
Mir Mozin <moazinmir@gmail.com>
2024-11-02 22:10:17 +0530

    Push files via Git

--- src/components/Chamber.jsx
+++ src/components/Chamber.jsx
@@ -3,7 +3,7 @@ import './Cards.css'
 const Chamber = () => {
   return (
     <div className='card'> 
-        <img src="/src/images/img1.png"/>
+        <img src="/images/img1.png"/>
         <h1>Chamber</h1>
         <h3>Sentinel</h3>
         <p>Well dressed and well armed, French weapons designer Chamber expels aggressors with deadly precision. He leverages his custom arsenal to hold the line and pick off enemies from afar, with a contingency built for every plan.</p>        
--- src/components/Jett.jsx
+++ src/components/Jett.jsx
@@ -3,7 +3,7 @@ import './Cards.css'
 const Jett = () => {
   return (
     <div className='card'> 
-        <img src="/src/images/img3.jpg"/>
+        <img src="/images/img3.jpg"/>
         <h1>Jett</h1>
         <h3>Duelist</h3>
         <p>Representing her home country of South Korea, Jett&apos;s agile and evasive fighting style lets her take risks no one else can. She runs circles around every skirmish, cutting enemies before they even know what hit them.</p>        
